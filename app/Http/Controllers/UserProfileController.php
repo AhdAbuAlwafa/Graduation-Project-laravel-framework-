c79db6a26f4b5a280c345f0f65c61c785d308723
AhdabuaLwafa <abwalwfahd@gmail.com>
2023-06-03 17:25:32 +0300

    AHD

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -43,9 +43,10 @@ class UserProfileController extends Controller
        
         $user=User::with('crafts','addresses',)->where('id',$id)->first();
         $cities = Address::pluck('city_name', 'id');
-
-        return view('userPage.userProfile',compact('user','cities'));
+        $village = Address::pluck('village_name', 'id');
+        return view('userPage.userProfile',compact('user','cities','village'));
     }
+    
 
     /**
      * Show the form for editing the specified resource.
@@ -64,20 +65,31 @@ class UserProfileController extends Controller
         $validated = $request->validate([
             'fname' => 'required',
             'lname' => 'required',
-            'number'=>'required|regex:/(05)[69][0-9]{7}/|unique:users'
+            'number'=>'required|regex:/(05)[69][0-9]{7}/|unique:users',
+            'password'=>'required'
             
         ]);
-   
-        $user = User::findOrFail($id);
+        $address=Address::where('village_name',$request->village_name)->first();
 
-        $user->fname = $request->input('fname');
-        $user->lname = $request->input('lname');
-        $user->number = $request->input('number');
-        $user->address->city_name = $request->input('city_name');
-
-        $user->save();
+        user::where('id',$id)->update([
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+            'number' => $request->number,
+            'password'=>$request->password,
+            'address_id'=>$address->id
+        ]);
         return redirect(route('userPage.userProfile', $id ));
-
+    
+/**
+         * $addressId=Address::get('id')->where('village_name',$request->village_name);  
+         *  user::find($id)->with('addresses')->update([
+         * 'fname' => $request->fname,
+         *             'lname' => $request->lname,
+         * 'number' => $request->number,
+         *  'password'=>$request->password,
+         * 'address_id'=>$addressId
+         *  ]);
+         */
     }
     /**
      * Remove the specified resource from storage.
--- app/Models/Address.php
+++ app/Models/Address.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
+    protected $fillable = [
+        
+        'city_name',
+        'village_name' 
+    
+        
+    ];
+
     
     use HasFactory;
     public function user(){
