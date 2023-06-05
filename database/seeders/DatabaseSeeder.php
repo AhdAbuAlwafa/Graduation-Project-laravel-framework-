f046305d872a82de0f3bf8d50de9da681065ec26
hayaobaid <hayaobaid007@gmail.com>
2023-06-05 15:59:45 -0700

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +14,25 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        
+        $addresses = \App\Models\Address::factory()->create([
+            'city_name' => 'Jenin',
+            'village_name' => 'obaid',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'fname' => 'haya',
+            'lname' => 'obaid',
+            'number' => 12345,
+            'image' =>'',
+            'description'=> '',
+            'num_evl'=> 0,
+            'all_evl'=> 0,
+            'gender'=> 0,
+            'is_worker'=> 0,
+            'is_active'=> 0,
+            'address_id'=> $addresses->id, 
+            'password' => Hash::make('12345')
+        ]);
     }
 }
