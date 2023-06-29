2291521dba6788d58530ecdafbc100c2e1b08324
AhdabuaLwafa <abwalwfahd@gmail.com>
2023-06-29 22:01:25 +0300

    add regester backend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,15 +4,9 @@
 
 <link rel="stylesheet" href="assets/css/login.css">
 
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
 
 <body>
-    <script src="js/cdnjs.cloudflare.com_ajax_libs_popper.js_1.11.0_umd_popper.min.js"></script>
-    <script src="js/code.jquery.com_jquery-3.7.0.min.js"></script>
-    <script src="js/bootstrap.js"></script>
+   
     <div class="continer" dir="rtl">
         <div class="box">
             <form method="POST" action="{{ route('login') }}">
@@ -65,22 +59,32 @@
             </form>
 
             <!--------------- regester--------->
+                
+     <form action="{{route('register')}}" method="post" id="regform">
+        @csrf 
             <div class="box-register" id="register">
 
                 <div class="top-header">
                     <h3>قم بانشاء حسابك الان ! </h3>
                     <small>نحن سعيدون بانضمامك الينا</small>
                 </div>
+                <span class="error-text fname_error"></span>
+                <span class="error-text lname_error"></span>
+                <span class="error-text phone_error"></span>
+                <span class="error-text description_error"></span>
+                <span class="error-text password_error"></span>
+                <span class="error-text gender_error"></span>
+    
                 <!-------------------------اسم الشخص------------------------------------------>
                 <div class="row">
                     <div class="col">
                         <div class="input-field2">
-                            <input type='text' class='input-box2' placeholder="الاسم الاول" id="reg-user" required>
+                            <input type='text' class='input-box2' placeholder="الاسم الاول" name="fname" id="reg-user" required>
                         </div>
                     </div>
                     <div class="col">
                         <div class="input-field-left">
-                            <input type='text' class='input-box2' placeholder="اسم العائلة" id="reg-family" required>
+                            <input type='text' class='input-box2' placeholder="اسم العائلة" name="lname" id="reg-family" required>
                         </div>
                     </div>
                 </div>
@@ -92,8 +96,11 @@
                     <div class="col">
                         <div class="input-field2">
                             <div class="select-country">
-                                <select class='input-box2' style="color: #4e4e4e;">
-                                    <option> القريه</option>
+                                <select class='input-box2' id="village" name="village" style="color: #4e4e4e;">
+                                    @foreach ($address as $item)
+                                    <option value="{{$item->id}}">{{$item->village_name}}</option>
+            
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
@@ -101,8 +108,11 @@
                     <div class="col">
                         <div class="input-field-left">
                             <div class="select-country">
-                                <select class='input-box2' style="color: #4e4e4e;">
-                                    <option> المدينه</option>
+                                <select class='input-box2' id="city" name="city" style="color: #4e4e4e;">
+                                    @foreach ($address as $item)
+                                    <option value="{{$item->id}}">{{$item->city_name}}</option>
+            
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
@@ -113,33 +123,37 @@
                     <div class="col">
                         <div class="input-field2">
                             <div class="select-country">
-                                <select class='input-box2' style="color: #4e4e4e;">
-                                    <option>المهنه</option>
+                                <select class='input-box2' id="crafts" name="craft" style="color: #4e4e4e;">
+                                    @foreach ($crafts as $item)
+                                    <option value="{{$item->id}}">{{$item->craft_name}}</option>
+        
+                                    @endforeach
+        
                                 </select>
                             </div>
                         </div>
                     </div>
                     <div class="col">
                         <div class="input-field-left">
-                            <input type='text' class='input-box2' placeholder=" رقم الهاتف" id="phone" required>
+                            <input type='text' class='input-box2' placeholder=" رقم الهاتف" id="phone" name="number" required>
                         </div>
                     </div>
                 </div>
                 <!--------------------------------------تjob-desc------------------------------------------------->
                 <div class="input-field-left">
-                    <input type='text' class='input-box3' placeholder="وصف المهنه " id="reg-user" required>
+                    <input type='text' class='input-box3' placeholder="وصف المهنه " id="reg-description"  name="description" required>
                 </div>
                 <!-------------------------------------password and confirm--------------------->
                 <div class="row">
                     <div class="col">
                         <div class="input-field2">
-                            <input type='text' class='input-box2' placeholder=" كلمة السر" id="reg-user" required>
+                            <input type='text' class='input-box2' placeholder=" كلمة السر" id="reg-user" name="password" required>
                         </div>
                     </div>
                     <div class="col">
                         <div class="input-field-left">
                             <input type='text' class='input-box2' placeholder="تاكيد كلمة السر " id="reg-family"
-                                required>
+                            name="password_confirmation" required>
                         </div>
                     </div>
                 </div>
@@ -149,11 +163,11 @@
                     <h2>الجنس</h2>
                     <div class="gender-option">
                         <div class="gender">
-                            <input type="radio" id="check-male" name="gender" checked />
+                            <input type="radio" id="check-male" name="gender" value="0" checked/>
                             <label for="check-male"> انثى</label>
                         </div>
                         <div class="gender">
-                            <input type="radio" id="check-female" name="gender" />
+                            <input type="radio" id="check-female" name="gender" value="1" />
                             <label for="check-female"> ذكر</label>
                         </div>
                     </div>
@@ -176,23 +190,75 @@
                 <div class="btn" id="btn"></div>
             </div>
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
         <script>
             var x = document.getElementById('login');
             var y = document.getElementById('register');
             var z = document.getElementById('btn');
-
+    
             function login() {
                 x.style.left = "3px";
                 y.style.right = "-500px";
-                z.style.left = "205px";
+                z.style.left = "200px";
             }
             function register() {
                 x.style.left = "-500px";
                 y.style.right = "45px";
                 z.style.left = "0px";
             }
+            //ajax
+    
+            $(function(){
+                $('#regform').on('submit',function(e){
+                    e.preventDefault();
+    
+                    $.ajax({
+                        url:$(this).attr('action'),
+                        method:$(this).attr('method'),
+                        data: new FormData(this),
+                        processData:false,
+                        dataType:'json',
+                        contentType:false,
+                        beforSend:function(){
+                            $(document).find('span.error-text').text('');
+                        },
+                        success:function(data){
+                            if(data.status == 0 ){
+                                $.each(data.error,function(prefix,val){
+                                    $('span.'+prefix+'_error').text(val[0]);
+    
+                                });
+                            }else{
+                                $('#regform')[0].reset[0]
+                            }
+                        }
+    
+                    })
+    
+                })
+            })
+    
+    
+    
         </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
 </body>
 @endsection
