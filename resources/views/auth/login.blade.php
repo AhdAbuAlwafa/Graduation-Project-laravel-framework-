033f555cb85860755030fe8f0f4c85d29c18ba54
hayaobaid <hayaobaid007@gmail.com>
2023-07-12 13:17:41 -0700

    change the navbar and add corsal to home page
    Signed-off-by: hayaobaid <hayaobaid007@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<!DOCTYPE html>
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
 <link rel="stylesheet" href="assets/css/login.css">
-
-
+<link href="{{ asset('assets/css/navbar.css') }}" rel="stylesheet">
+        @include('shared.navbar')
 <body>
-
-    <div class="continer" dir="rtl">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    <div class="connn" dir="rtl">
         <div class="box">
             <form method="POST" action="{{ route('login') }}">
                 @csrf
@@ -54,6 +63,7 @@
                         <div class="forget">
                             <a href="#">هل نسيت كلمة السر؟ </a>
                         </div>
+                        
                     </div>
                 </div>
             </form>
@@ -64,7 +74,7 @@
                 @csrf
                 <div class="box-register" id="register">
 
-                    <div class="top-header">
+                    <div class="top-header2">
                         <h3>قم بانشاء حسابك الان ! </h3>
                         <small>نحن سعيدون بانضمامك الينا</small>
                     </div>
@@ -129,34 +139,6 @@
 
                     </div>
 
-
-                    <!--------------------------------مكانه row---------------------->
-                    <div class="row">
-                        <div class="col">
-                            <div class="input-field2">
-                                <div class="select-country">
-                                    <select class='input-box2' id="village" name="village" style="color: #4e4e4e;">
-                                        @foreach ($address as $item)
-                                        <option value="{{$item->id}}">{{$item->village_name}}</option>
-
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="input-field-left">
-                                <div class="select-country">
-                                    <select class='input-box2' id="city" name="city" style="color: #4e4e4e;">
-                                        @foreach ($address as $item)
-                                        <option value="{{$item->id}}">{{$item->city_name}}</option>
-
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
                     <!-------------------num------------------------------->
                     <div class="row">
@@ -230,7 +212,18 @@
                             </div>
                         </div>
                     </div>
-
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                        <label class="form-check-label" for="flexRadioDefault1">
+                          Default radio
+                        </label>
+                      </div>
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                        <label class="form-check-label" for="flexRadioDefault2">
+                          Default checked radio
+                        </label>
+                      </div>
                     <!---------------------------------submit------------------------------------->
                     <div class="input-field">
                         <button type="submit" class="input-submit2">
@@ -336,4 +329,5 @@
         </script>
     </div>
 </body>
+</html>
 @endsection
--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="navbar">
+<!--<div class="navbar">
     <div class="logo">
         <label>Call Mehani</label>
     </div>
@@ -31,4 +31,84 @@
 
         </ul>
     </nav>
-</div>
+</div>--->
+<!DOCTYPE html>
+<html lang="en">
+<!--divinectorweb.com-->
+<head>
+	<meta charset="UTF-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<title>homepage</title>
+	<!-- All CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+	<link href="nav.css" rel="stylesheet">
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		<div class="container">
+			<div class="collapse navbar-collapse" id="navbarSupportedContent" >
+				<ul class="navbar-nav ms-auto mb-2 mb-lg-0" >
+                    <a class="navbar-brand"  href="#"><span class="text-warning" >CALL</span >MEHANI</a> <button  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
+                   <!----------فراغ اللوجو----->
+                    <li class="nav-item">
+						<a class="nav-link" style="color: rgb(0, 0, 0);" href='{{ route('userPage.userProfile') }}'></a>
+					</li>
+                    <li class="nav-item">
+						<a class="nav-link" style="color: rgb(0, 0, 0);" href='{{ route('userPage.userProfile') }}'></a>
+					</li>
+                    <li class="nav-item">
+						<a class="nav-link" style="color: rgb(0, 0, 0);" href='{{ route('userPage.userProfile') }}'></a>
+					</li>
+                    <li class="nav-item">
+						<a class="nav-link" style="color: rgb(0, 0, 0);" href='{{ route('userPage.userProfile') }}'></a>
+					</li>
+                    <li class="nav-item">
+						<a class="nav-link" style="color: rgb(0, 0, 0);" href='{{ route('userPage.userProfile') }}'></a>
+					</li>
+                    
+                    <!------------------------------------->
+                    @if (auth()->user()!= null)
+                    <li class="nav-item" >
+                        <a class="nav-link" style="color: rgb(0, 0, 0);" href="{{ route('logout') }}" onclick="event.preventDefault() ; document.getElementById('logout').submit()" class=" btn nav-btn">تسجيل الخروج</a>
+                    </li>
+                    <form action="{{ route('logout') }}" hidden disabled id="logout"  method="post">
+                        @method('post')
+                        @csrf
+                    </form>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" style="color: rgb(0, 0, 0);"  href='/login'>تسجيل الدخول</a>
+                        </li>
+                    @endif
+                    <li class="nav-item">
+						<a class="nav-link"  style="color: rgb(0, 0, 0);" href='{{ route('who-are-we') }}'>من نحن</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="color: rgb(0, 0, 0);" href='{{ route('userPage.userProfile') }}'>حسابي</a>
+					</li>
+                        <li class="nav-item">
+                            <a class="nav-link"style="color: rgb(0, 0, 0);" href='{{ route('userPage.getAllUser') }}''>المهن</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" style="color: rgb(0, 0, 0);" href='#'>اعلانات العمل</a>
+                        </li>
+                        <li class="nav-item" >
+                            <a class="nav-link" style="color: rgb(0, 0, 0);" href='/'>الرئيسية</a>
+                        </li>
+				</ul>
+			</div>
+            
+		</div>
+	</nav>
+	
+	<!-- footer ends -->
+	
+	<!-- All Js -->
+	
+</body>
+</html>
