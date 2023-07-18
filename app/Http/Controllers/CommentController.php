a6f77d051950e77a5ede76b0b88c06ef6d7dd08d
Rahafabushamleh <rahaf.abushamleh@gmail.com>
2023-07-18 22:30:06 +0300

    add CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Rate;
+use App\Models\User;
 use App\Models\User_Comment;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\New_;
@@ -27,24 +30,48 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        dd($request);
-
-        $validated = $request->validate([
-          
-            'com_text' => 'required',
-            
-        ]);
-        $user_comments=New User_Comment;
-        $user_comments->com_text =$request->input('com_text');
-        $user_comments->save();
-
-
-
-
-
-    }
+   
+     public function store(Request $request , $id)
+     {
+ 
+         $user = User::find($id)->first();
+         $receivedComments = User_Comment::get()->where('worker_id',$id);
+         $isRated = Rate::where('reviewable',$id)->where('reviewer',auth()->user()->id)->first();
+         $worker = User::with('addresses', 'crafts')->where('id', $id)->first();
+         $totalRate = Rate::where('reviewable',$id)->sum('rate');
+         $totalReviewers =  Rate::where('reviewable',$id)->count();
+         if($totalReviewers == 0 or $totalRate == 0 ){
+             $userRate = 0;
+         }else {
+             $userRate = $totalRate / $totalReviewers;
+         }
+ 
+ 
+         $validated = $request->validate([
+             'com_text' => 'required',
+         ]);
+     
+         $user = auth()->user(); 
+         $workerId = User::find($id); 
+         
+     
+         // $worker = User::findOrFail($id); // Retrieve the worker model
+     
+         $user_comments = new User_Comment;
+         $user_comments->com_text = $request->input('com_text');
+     
+         $user_comments->user()->associate($user);
+         $user_comments->worker()->associate($worker); // Associate the worker model
+     
+         $user_comments->save(); 
+ 
+         // Prepare the HTML for the new comment
+         $commentHtml = view('userPage.partial', ['comment' => $user_comments])->render();
+     
+         // Return the JSON response with the comment HTML
+         return response()->json(['commentHtml' => $commentHtml]);
+     }
+ 
 
     /**
      * Display the specified resource.
